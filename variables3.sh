d1626b6aba6a2f80e9dffcedd613e90ab45b3041
vineetha <seelamvineetha@gmail.com>
2025-11-26 23:01:46 +1100

    variables file added

--- variables3.sh
+++ variables3.sh
@@ -8,3 +8,7 @@ echo "$PERSON2:: Hello $PERSON1, I am fine. How are you doing"
 echo "$PERSON1:: I am fine too. What's up?"
 echo "$PERSON2:: Nohting, just going to Mars now, will you come?"
 echo "$PERSON1:: Sorry, you carry on! I will come once you come back"
+
+sleep 10 &
+
+echo " PID of last background command is $! "
