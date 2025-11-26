cbee0ed8e1aa696a798a16455d4608ea0b4ad7e5
vineetha <seelamvineetha@gmail.com>
2025-11-27 10:33:41 +1100

    variables file added

---
+++ installmysql.sh
@@ -0,0 +1,30 @@
+#/bin/bash
+USRID=$(id -u)
+
+if [ $USRID -ne 0 ]
+then
+ 
+    echo "ERROR:: please sign in with root user "
+    exit 1
+else 
+    echo "already root user "
+fi
+
+dnf list install mysql 
+
+if [ $? -ne 0]
+then 
+ 
+    echo " msq not installed..going to install "
+    dnf install mysql -y
+    if [$? -eq 0]
+    then
+        echo " mysql installed successfully "
+    else
+        echo " installing mysql is failed "
+        exit 1
+
+else 
+    echo " already installed "
+    
+if
