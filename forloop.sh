0ac97cf1f1e7d36878b535a339df39167fb9557b
vineetha <seelamvineetha@gmail.com>
2025-11-28 09:37:43 +1100

    new sh file commited

--- forloop.sh
+++ forloop.sh
@@ -1,4 +1,42 @@
-for i in {1..100}
-do
-    echo $i
+
+#!/bin/bash
+USRID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+B="\e[33m"
+N="\e[0m"
+PACKAGES=("mysql" "python3" "nginx" "httpd")
+
+
+if [ $USRID -ne 0 ]
+then
+    echo -e "$R ERROR:: please sign in with root user $n"
+    exit 1
+else 
+    echo "already root user"
+fi
+VALIDATE(){
+    if [ $1 -eq 0 ]
+    then
+        echo -e "$G $2 installed successfully $N"
+    else
+        echo "installing $2 is failed"
+        exit 1
+    fi
+
+}
+
+for i in ${PACKAGES[@]}
+do 
+
+    dnf list installed $i 
+    if [ $? -ne 0 ]
+    then 
+        echo "$i not installed..going to install"
+        dnf install $i -y
+        VALIDATE $? "$i"
+    else 
+        echo -e "$B already installed $N"
+fi
 done
+
