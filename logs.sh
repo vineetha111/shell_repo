a12f5642325f019fa54deae09e9acd9f7a8d3688
vineetha <seelamvineetha@gmail.com>
2025-11-27 22:00:00 +1100

    added new file

--- logs.sh
+++ logs.sh
@@ -6,13 +6,10 @@ B="\e[33m"
 N="\e[0m"
 LOGS_FOLDER="/var/log/shellscript-logs"
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-LOG_FILE="$LOG_FOLDER/$SCRIPT_NAME.log"
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
 
 mkdir -p $LOGS_FOLDER
 echo "script started executing at : $(date)" &>>$LOG_FILE
-
-
-
 
 if [ $USRID -ne 0 ]
 then
