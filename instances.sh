02ed92fa7cee617be4f84a75698412af8f006153
vineetha <seelamvineetha@gmail.com>
2025-11-28 10:32:49 +1100

    new sh file commited

--- instances.sh
+++ instances.sh
@@ -6,8 +6,8 @@ INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipp
 ZONE_ID="Z02643283SYC8C4HUQCUK" # replace with your ZONE ID
 DOMAIN_NAME="vinnimakeovers.online" # replace with your domain
 
-#for instance in ${INSTANCES[@]}
-for instance in $@
+for instance in ${INSTANCES[@]}
+#for instance in $@
 do
     INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-019ecaf22c9101836 --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
     if [ $instance != "frontend" ]
